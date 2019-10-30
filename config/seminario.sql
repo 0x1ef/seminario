e49562ceb5dd5df8a7c0c7390a77396d8c815569
yamil <yamilgallegos.e@gmail.com>
2019-10-30 13:55:06 -0300

    actualizacion base de datos

--- config/seminario.sql
+++ config/seminario.sql
@@ -2,7 +2,7 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.5.5-10.1.36-MariaDB : Database - seminario
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -16,23 +16,76 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`seminario` /*!40100 DEFAULT CHARACTER S
 
 USE `seminario`;
 
-/*Table structure for table `archivos` */
+/*Table structure for table `archivo` */
 
-DROP TABLE IF EXISTS `archivos`;
+DROP TABLE IF EXISTS `archivo`;
 
-CREATE TABLE `archivos` (
+CREATE TABLE `archivo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(30) DEFAULT NULL,
-  `descripcion` varchar(50) DEFAULT NULL,
-  `ruta` varchar(200) DEFAULT NULL,
-  `tipo` varchar(20) DEFAULT NULL,
-  `size` int(11) DEFAULT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
+  `size` float NOT NULL,
+  `tarea` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `archivo_fk` (`tarea`),
+  CONSTRAINT `archivo_fk` FOREIGN KEY (`tarea`) REFERENCES `tarea` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `archivo` */
+
+/*Table structure for table `perfil` */
+
+DROP TABLE IF EXISTS `perfil`;
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-/*Data for the table `archivos` */
+/*Data for the table `perfil` */
 
-insert  into `archivos`(`id`,`nombre`,`descripcion`,`ruta`,`tipo`,`size`) values (4,'prueba02.txt','prueba con nombre','C:/xampp/htdocs/www/POO/productos/archivo/','text/plain',0),(5,'prueba.cpp','prueba con nombre','C:/xampp/htdocs/www/POO/productos/archivo/','application/octet-st',1175),(6,'prueba.txt','prueba de cargar','C:/xampp/htdocs/www/POO/productos/archivo/','text/plain',8),(7,'prueba.o','prueba de cargar2','C:/xampp/htdocs/www/POO/productos/archivo/','application/octet-st',68831),(8,'prueba.txt','prueba iguales','C:/xampp/htdocs/www/POO/productos/archivo/','text/plain',7);
+insert  into `perfil`(`id`,`nombre`) values (1,'administrador'),(2,'comun');
+
+/*Table structure for table `tarea` */
+
+DROP TABLE IF EXISTS `tarea`;
+
+CREATE TABLE `tarea` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tarea_fk` (`usuario`),
+  CONSTRAINT `tarea_fk` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tarea` */
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) NOT NULL,
+  `apellido` varchar(200) NOT NULL,
+  `correo` varchar(200) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `tipodeusuario` int(11) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `nombreusuario` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario_un` (`nombreusuario`),
+  KEY `usuario_fk` (`tipodeusuario`),
+  CONSTRAINT `usuario_fk` FOREIGN KEY (`tipodeusuario`) REFERENCES `perfil` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `usuario` */
+
+insert  into `usuario`(`id`,`nombre`,`apellido`,`correo`,`pass`,`tipodeusuario`,`estado`,`nombreusuario`) values (2,'admin','prueba','admin@prueba.com','123456',1,1,'admin');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
