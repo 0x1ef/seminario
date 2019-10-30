1651dc4562d1f4dce3aee9f03ba8c850c7307a80
yamil <yamilgallegos.e@gmail.com>
2019-10-30 16:15:14 -0300

    se corrige un erro de redireccion
    ahora redireccion al index correcto si la sesion no esta iniciada

--- view/administrador.php
+++ view/administrador.php
@@ -3,11 +3,12 @@
 	session_start();
 	
 	if(!isset($_SESSION["usuario"])){
-		header("Location:index.html");
+		header("Location:../index.php");
 	}
 ?>
+<header><h1>Panel de administracion</h1></header>
 <main>
-	<h1>Panel de administracion</h1>
+	<p><a href="index.php?c=usuario&a=consultarUsuariop" class="btn btn-primary">Consultar Usuarios pendientes</a><p>
 	
 </main>
 </html>
