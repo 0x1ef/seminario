bcb360ad8f3578ffe511a030bcee72247f7ea63e
Debian <yamilgallegoos.e@gmail.com>
2020-01-27 23:45:11 -0300

    Se agrego el front de el usuario comun
    Sin funcionalidad

--- view/usuarioComun.php
+++ view/usuarioComun.php
@@ -1,2 +1,36 @@
-<header><h1>Administracion de tareas</h1><header>
-<a href=></a>
+<header><h1>Administracion de tareas</h1></header>
+<div>
+  <div class="row">
+    <div class="col-2">
+    	<h1>Menu</h1>
+    	<div>
+    		<nav class="nav nav-pills flex-column">
+ 				<a class="nav-link active" href="#">Crear Tarea</a>
+  				<a class="nav-link" href="#">Listar Tareas</a>
+ 				 <a class="nav-link" href="#">Link</a>
+			</nav>
+    	</div>
+    </div>
+    <div class="col-6">
+    	<div class="centrar">
+    		<h1>Cargar Tarea</h1>
+    		<div>
+    			<form method="post" action="" class="px-6 py-4">
+    				<div class="form-group row">
+    					<div class="col-sm-4">
+    						<label for="titulo">Titulo o Descripcion</label>
+    						<input type="text" name="titulo" id="titulo" class="form-control">
+    					</div>
+    				</div>
+    				<div class="form-group row">
+    					<div class="col-sm-4">
+    						<label for="desc">Archivo</label>
+    						<input type="file" name="descripcion" id="desc">
+    					</div>
+    				</div>
+    			<form>
+    		</div>
+    	</div>
+    </div>
+  </div>
+</div>
