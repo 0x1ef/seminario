ab696effcd6700629e95cf33c6acdfb347fbad6a
Debian <yamilgallegoos.e@gmail.com>
2020-02-05 23:11:24 -0300

    Se modifico la interfaz de consultar usuarios

--- view/consultarUsuarios.php
+++ view/consultarUsuarios.php
@@ -4,10 +4,9 @@
 		header("Location:../index.php");
 	}
 	echo "<header><h1>Usuarios</h1></header>";
-	echo "<form method='post' action='index.php?c=usuario&a=modificar'>";
 	echo "<table class='table table-bordered>'";
 	echo "<tr class='table-primary'>";
-	echo "<th>Nombre</th><th>apellido</th><th>correo</th><th>nombre de usuario</th><th>estado</th><th>Seleccionar</th>";
+	echo "<th>Nombre</th><th>apellido</th><th>correo</th><th>nombre de usuario</th><th>estado</th><th>Accion</th>";
 	echo "</tr>";
 	foreach ($datos as $valor){
 		echo "<tr>";
@@ -16,13 +15,10 @@
 		echo "<td>".$valor['correo']."</td>";
 		echo "<td>".$valor['nombreusuario']."</td>";
 		echo "<td>".$valor['estado']."</td>";
-		echo "<td><input type='radio' name='validar' value='".$valor['id']."'></td>";
+		echo "<td> <a href='index.php?c=usuario&a=modificar'> <i class='fas fa-edit fa-2x'></i></a> <a href='index.php?c=usuario&a=eliminar'><i class='fas fa-trash-alt fa-2x'></i></a> </td>";
 		echo "</tr>";
 	}
 	echo "</table>";
-	echo "<button type='submit' class='btn btn-success'>Modificar</button>";
-	echo "<button type='submit' formaction='index.php?c=usuario&a=eliminar' class='btn btn-danger'>Eliminar</button>";
-	echo "</form>";
 	echo "<a href='index.php' class='btn btn-primary'>volver</a>";
 	
 ?>
