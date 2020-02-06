68bce512fb043d537ce13bff8290229460e03544
Debian <yamilgallegoos.e@gmail.com>
2020-02-05 23:10:31 -0300

    Se agrego la nueva hoja de estilos

--- view/header.php
+++ view/header.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/estilos.css">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     </head>
 <body>
     <header>
