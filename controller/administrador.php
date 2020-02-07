b3e31332ea36147f872a93890271925ee441fa1a
Debian <yamilgallegoos.e@gmail.com>
2020-02-07 14:27:14 -0300

    Se agrego un mensaje al eliminar un usuario
    un alert en js

--- controller/administrador.php
+++ controller/administrador.php
@@ -6,10 +6,19 @@ class AdministradorController{
 	public function __CONSTRUCT(){
         $this->model = new Administrador();
     }
+
     public function index(){
     	require_once 'view/header.php';
     	require_once 'view/administrador.php';
     	require_once 'view/footer.php';
+    }
+
+    public function eliminar(){
+    	$this->model->id = $_REQUEST['id'];
+    	$this->model->eliminar();
+        echo "<script>alert('El usuario se elimino correctamente')</script>";
+        echo "<meta http-equiv='refresh' content='0; url=index.php?c=usuario&a=consultarUsuario'>";
+    	
     }
 }
 ?>
