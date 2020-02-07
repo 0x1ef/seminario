bf76470398c95bf91c0d5faa03c80f939b795235
Debian <yamilgallegoos.e@gmail.com>
2020-02-07 17:37:20 -0300

    se agrego el metodo modificar

--- controller/administrador.php
+++ controller/administrador.php
@@ -20,5 +20,17 @@ class AdministradorController{
         echo "<meta http-equiv='refresh' content='0; url=index.php?c=usuario&a=consultarUsuario'>";
     	
     }
+
+    public function modificar(){
+        $this->model->id= $_REQUEST['id'];
+        if(empty($this->model->id) || !$this->model->existe()){
+            header ('location: index.php?c=usuario&a=consultarUsuario');
+        }else{
+            $this->model->obtenerDatos();
+            require_once 'view/header.php';
+            require_once 'view/modificar.php';
+            require_once 'view/footer.php';
+        }
+    }
 }
 ?>
