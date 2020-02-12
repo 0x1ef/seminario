f16323993176206e58857fbaa264f556bc10ae71
Debian <yamilgallegoos.e@gmail.com>
2020-02-11 21:09:00 -0300

    Se agregaron metodos Get y set

--- model/administrador.php
+++ model/administrador.php
@@ -40,6 +40,75 @@ class Administrador{
 			return false;
 		}
 	}
+
+	public function obtenerDatos(){
+		$sql="SELECT nombre,apellido,correo,tipodeusuario,nombreusuario FROM usuario WHERE id=:id";
+		$resultado=$this->pdo->prepare($sql);
+		$resultado->bindValue(':id',$this->id);
+		$resultado->execute();
+		$tmp = $resultado->fetchAll();
+		return $tmp;
+	}
+
+	public function actualizarD(){
+		
+	}
+
+	public function setNombre($nombre){
+		$this->nombre=$nombre;
+	}
+
+	public function setApellido($apellido){
+		$this->apellido=$apellido;
+	}
+
+	public function setCorreo($correo){
+		$this->correo=$correo;
+	}
+
+	public function setNombreUsuario($nombreUsuario){
+		$this->nombreUsuario=$nombreUsuario;
+	}
+
+	public function setPass($pass){
+		$this->pass=$pass;
+	}
+
+	public function setTipo($tipo){
+		$this->tipodeusuario=$tipo;
+	}
+
+	public function setId($id){
+		$this->id=$id;
+	}
+
+	public function getNombre(){
+		return $this->nombre;
+	}
+
+	public function getApellido(){
+		return $this->apellido;
+	}
+
+	public function getCorreo(){
+		return $this->correo;
+	}
+
+	public function getNombreUsuario(){
+		return $this->nombreUsuario;
+	}
+
+	public function getPass(){
+		return $this->pass;
+	}
+
+	public function getTipo(){
+		return $this->tipodeusuario;
+	}
+
+	public function getId(){
+		return $this->id;
+	}
 }
 
 ?>
