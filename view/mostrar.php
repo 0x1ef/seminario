4557ce420eb6f0876eb6b04d2aa22218b31ba284
Debian <yamilgallegoos.e@gmail.com>
2020-03-09 16:52:06 -0300

    se agrego la vista de las tareas

--- view/mostrar.php
+++ view/mostrar.php
@@ -1,22 +1,23 @@
 <?php
-    echo "<h1>Administrar Archivos</h1>";
+    echo "<header><h1>Administrar Archivos</h1></header>";
 
 	echo "<form method='POST' action='index.php?c=archivo&a=ejecutar'>";
     echo "<table class='table table-bordered'>";
     echo "<tr class='table-primary'>";
-    echo "<th>Nombre</th><th>Descripcion</th><th>Tipo</th><th>Seleccionar</th>";
+    echo "<th>Nombre</th><th>Descripcion</th><th>estado</th><th>Seleccionar</th>";
     echo "</tr>";
 	foreach ($resultado as $valor){
         echo "<tr>";
-        echo "<td>".$valor['nombre']."</td>"; 
+        echo "<td>".$valor['titulo']."</td>"; 
 		echo "<td>".$valor['descripcion']."</td>";
-        echo "<td>".$valor['tipo']."</td>";
-		echo "<td><input type='radio' name='ejecutar' value='".$valor['nombre']."'></td>";
+        echo "<td>".$valor['estado']."</td>";
+		echo "<td><input type='radio' name='ejecutar' value='".$valor['titulo']."'></td>";
+        echo "<input type='hidden' name=idusuario value='".$valor['usuario']."'";
         echo "</tr>";
 	}
     echo "</table>";
 	echo "<input class='btn btn-primary' type='submit' value='ejecutar'>";
-	echo "<a href='index.php' class='btn btn-primary'>Volver</a>";
+	echo "<a href='index.php?c=usuariocomun&a=index' class='btn btn-primary'>Volver</a>";
 	echo "</form>";
 
 ?>
