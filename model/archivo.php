d5154a944babad3a5dc79f7ccc289f12cf09629c
Debian <yamilgallegoos.e@gmail.com>
2020-03-09 16:51:43 -0300

    Se agrego el metodo obtenerUsr

--- model/archivo.php
+++ model/archivo.php
@@ -27,7 +27,7 @@ class Archivo
 		try
 		{
 			$result = array();
-			$stm = $this->pdo->prepare("SELECT * FROM archivos");
+			$stm = $this->pdo->prepare("SELECT * FROM tarea");
 			$stm->execute();
 
 			return $stm->fetchAll();
@@ -51,5 +51,20 @@ class Archivo
 		}catch(Exception $e){
 			die("Erro no se cargo el archivo".$e->getMessage());
 		}
+	}
+
+	public function ejecutar($nombre){
+		$sql="UPDATE tarea SET estado=2 where titulo='$nombre'";
+		$resultado=$this->pdo->prepare($sql);
+		$resultado->execute();
+	}
+
+	public function obtenerUsr($id){
+		$sql="SELECT nombreusuario FROM usuario where id=:id";
+		$resultado=$this->pdo->prepare($sql);
+		$resultado->bindValue(":id",$id);
+		$resultado->execute();
+		$var=$resultado->fetchAll();
+		return $var[0]['nombreusuario'];
 	}
 }
